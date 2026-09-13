e0827b1b33a243238372b4ea3d7dd2dcccd5ea38
remofilwemolehabangwe308-ops <remofilwemolehabangwe308@gmail.com>
2026-09-13 23:00:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,9 @@ while True:
     if option == 1:
         generate_data_file()
         print("Student data generated successfully")
+
     elif option == 2:
+        print("=== Class Data ===")
         students_data = load_students()
         for index, (name, score) in enumerate(students_data, start=1):
             student_id = f"S{index}"
@@ -39,20 +41,30 @@ while True:
         research_topic = "Statistics"
         honours_class_info= HonoursStudent(name, student_id, score, research_topic)
         print(honours_class_info)
+
     elif option == 3:
+        print("=== Passing Students ===")
         passing_data = passing_students(students_data)
         for name, score in passing_data:
             print(f"{name} | Score: {score}")
+
     elif option == 4:
+        print("=== Class Average ===")
         class_average_data = class_average(students_data)
         print(class_average_data)
+
     elif option == 5:
+        print("=== Highest Score ===")
         highest_score = highest(students_data)
         print(f"Highest score: {highest_score[0]} | Score: {highest_score[1]}")
+
     elif option == 6:
+        print("=== Lowest Score ===")
         lowest_score = lowest(students_data)
         print(f"Lowest score: {lowest_score[0]} | Score: {lowest_score[1]}")
+
     elif option == 7:
+        print("=== Pass Rate ===")
         if not students_data:
             print("No student data available. Please generate and load student data first.")
             continue
@@ -60,6 +72,7 @@ while True:
         print(data_pass_rate)
         print(f"Pass at 50: {grade_50(students_data[0][1])}")
         print(f"Pass at 80: {grade_80(students_data[0][1])}")
+
     elif option == 8:
         environment_report_data = environment_report()
         report_text = environment_report_data
@@ -67,9 +80,11 @@ while True:
         report_text = report_text + "\n" + date_report_data
         export_report(report_text)
         log_event("The report was exported")
-        print(report_text)
+        print(f'=== Enviroment And Date Report ===\n{report_text}')
+
     elif option == 9:
         print("Goodbye!!!")
         break
+
     else:
         print("Invalid option")  
